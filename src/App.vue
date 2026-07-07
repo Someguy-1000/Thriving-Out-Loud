911ca9088fe065ba9f2233309313813d747db962
Someguy-1000 <55808680+Someguy-1000@users.noreply.github.com>
2026-07-07 14:07:57 -0700

    added vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
 import Main from './components/Main.vue';
 import Shop from './components/Shop.vue';
 import AffiliateBanners from './components/AffiliateBanners.vue';
+import { Analytics } from "@vercel/analytics/vue";
 </script>
 
 <template>
+  <Analytics />
   <div class="scroll-smooth relative">
     <Main />
     <Shop />
